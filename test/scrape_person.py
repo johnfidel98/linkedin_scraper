085de957a06cf1ef7d8a7b2d1e47907baa7a0f79
Pawan Kumar Sharma <pawan1995219@gmail.com>
2022-09-07 14:49:48 +0530

    Update scrape_person.py

--- test/scrape_person.py
+++ test/scrape_person.py
@@ -1,5 +1,6 @@
 from linkedin_scraper import Person
 
 rick_fox = Person("https://www.linkedin.com/in/rifox?trk=pub-pbmap")
-iggy = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5")
+iggy = Person("https://www.linkedin.com/in/pawan-kumar-sharma-b179621bb")
 Anirudra = Person("https://in.linkedin.com/in/anirudra-choudhury-109635b1")
+print(iggy)
